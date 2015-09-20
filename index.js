19a4c74c98451b77d4516876394a0895282023d7
Shawn Campbell <scampbell@vetsfirstchoice.com>
2015-09-20 13:27:39 -0400

    returning json

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/jedi/:firstName/:lastname', function(req, res){
+  res.json({ name: req.params.firstName.substr(0,2) + req.params.lastname.substr(0,3) });
+});
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
